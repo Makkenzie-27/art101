ca9228c769be2728b34c11999c1073b69adf92d8
Makkenzie <makkenzie1@ymail.com>
2022-04-15 17:51:04 -0700

    adding Pseudocoding to lab4 html and js

---
+++ lab4/js/lab.js
@@ -0,0 +1,16 @@
+// Lab4: Pseudocing & Problem Solving.
+// Add a computer task Pseudocode to a javascript file in comments.
+// @https://makkenzie-27.github.io/art101/lab4/
+// @lab.js
+// @Makkenzie Zepeda.
+// @since  04.15.2022
+
+//Creating a Flappy-Bird Clone Game:
+//1.Print the background
+//2.Randomize Obstacles (has a set range)
+//3.Add player
+//4.Accept user Input (taps)
+//5.Evaluate movement
+//6.Start Score
+//7.Check for Player's hitbox
+//8.If Hitbox touches obstacle, end, otherwise, loop check for hitbox.
