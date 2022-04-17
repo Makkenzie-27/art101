099a0a4039e608f9dd09c67830fec5c9d720e1e6
Makkenzie <makkenzie1@ymail.com>
2022-04-17 10:22:54 -0700

    Lab 4 images added and css code

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -6,11 +6,11 @@
 // @since  04.15.2022
 
 //Creating a Flappy-Bird Clone Game:
-//1.Print the background
-//2.Randomize Obstacles (has a set range)
-//3.Add player
-//4.Accept user Input (taps)
-//5.Evaluate movement
-//6.Start Score
-//7.Check for Player's hitbox
-//8.If Hitbox touches obstacle, end, otherwise, loop check for hitbox.
+  //1.Print the background
+    //2.Randomize Obstacles (has a set range)
+      //3.Add player
+        //4.Accept user Input (taps)
+        //5.Evaluate movement
+  //6.Start Score
+  //7.Check for Player's hitbox
+  //8.If Hitbox touches obstacle, end, otherwise, loop check for hitbox.
