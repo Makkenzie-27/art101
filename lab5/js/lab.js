ef813dab38263a2fb1bf1c970fed8b695c01e4c3
Makkenzie <makkenzie1@ymail.com>
2022-04-19 16:03:25 -0700

    fixed years to year in the js file

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -21,5 +21,5 @@ document.writeln("Make: " + make + "<br>");
 document.writeln("Model: " + model + "<br>");
 document.writeln("Color: " + color + "<br>");
 document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age + "years<br>");
+document.writeln("Age: " + age + "year<br>");
 document.writeln("Own It: " + ownIt + "<br>");
