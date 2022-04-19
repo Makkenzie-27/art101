fcc8cca2e0c56264783af7182bb7722fb053f46e
Makkenzie <makkenzie1@ymail.com>
2022-04-19 15:37:47 -0700

    edited the lab 5 css, js, and html files

---
+++ lab5/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ *Author: Makkenzie Zepeda
+ *Created: April 19, 2022
+ *
+ *License: Public Domain
+ */
+
+ //Defining Variables
+ make = "Kia";
+ model = "Sportage";
+ color = "Silver";
+ year = 2021;
+ ownIt = true;
+
+
+//calculate
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + "years<br>");
+document.writeln("Own It: " + ownIt + "<br>");
