acf850a8eaffe502d99a74c48eda2a3941bbfe75
Makkenzie <makkenzie1@ymail.com>
2022-04-26 15:34:50 -0700

    changed lab7 js to lab.js & edited the html

---
+++ lab7/js/lab.js
@@ -0,0 +1,15 @@
+/**
+ *Author: Makkenzie Zepeda
+ *Created: April 26, 2022
+ *Lab07: Functions
+ *License: Public Domain
+ */
+
+// Define Variables
+
+
+
+
+
+//Output
+//document.writeln("Oh hey there's your name: "),
