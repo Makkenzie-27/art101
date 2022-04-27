13771e65f0c8667318450214a22c5427923221d7
Makkenzie <makkenzie1@ymail.com>
2022-04-26 18:03:20 -0700

    added the js text

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -5,11 +5,28 @@
  *License: Public Domain
  */
 
-// Define Variables
+// nameSorter - a function that takes user input and sorts the letters of their
+//name.
+function nameSorter(){
+  //collects the users name with a comment
+  var userName = window.prompt("Please give me your name.");
+  document.writeln("<b>"+userName +" </b>Nice name! <br>");
 
+  //split string array: makes the name into an array and prints the result
+  var splitArray = userName.split('');
+  document.writeln("Here's the plan. We're going to split up the name you gave us: "+splitArray+"<br>");
 
+  //sort the array we split
+  var sortArray = splitArray.sort();
+  document.writeln("sortArray = "+ sortArray +"<br>");
 
+  //join array back to a string
+  var nameJoined = sortArray.join('');
+  document.writeln("We collected your name, split it, sort it, and now rearranged it in alphabetical order! " +nameJoined+"<br>");
+  return nameJoined;
+}
 
 
 //Output
-//document.writeln("Oh hey there's your name: "),
+document.writeln("There you go your name is now: " ,
+  nameSorter(), "</br>");
