a94b7cbc7b3cc21a29d36e0de02b9dc181e358bf
Makkenzie <makkenzie1@ymail.com>
2022-04-26 19:19:24 -0700

    Updated js code

--- lab7/js/lab.js
+++ lab7/js/lab.js
@@ -9,24 +9,26 @@
 //name.
 function nameSorter(u){
   //prints the users input
-  document.writeln("<b>"+ u +" </b>Nice name! <br>");
+  document.writeln("<b>"+ u +"</b>, Nice name! <br><br>");
 
   //split string array: makes the name into an array and prints the result
   var splitArray = u.split('');
-  document.writeln("Here's the plan. We're going to split up the name you gave us: "+splitArray+"<br>");
+  document.writeln("Here's the plan.<br><br>");
+  document.writeln("We're going to split up the name you entered: "+"<b>"+splitArray+"</b><br><br>");
 
   //sort the array we split
   var sortArray = splitArray.sort();
-  document.writeln("sortArray = "+ sortArray +"<br>");
+  document.writeln("Now we are going to sort it alphabetically: "+ "<b>"+sortArray +"</b><br><br>");
 
   //join array back to a string
   var nameJoined = sortArray.join('');
-  document.writeln("We collected your name, split it, sort it, and now rearranged it in alphabetical order! " +nameJoined+"<br>");
+  document.writeln("Lets put your name back together shall we? <br><br> ");
+  document.writeln("Here is your name joined together: "+"<b>"+nameJoined+"</b><br><br>");
   return nameJoined;
 }
 
 
 //Output
-var userName = window.prompt("Please give me your name.");
-document.writeln("There you go your name is now: " ,
-  nameSorter(userName), "</br>");
+var userName = window.prompt("Please enter your name:");
+document.writeln("There you go your name is now: " +"<b>",
+  nameSorter(userName), "</b></br>");
