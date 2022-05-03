2e4da3f8e0aa8da956ad7a1655313e50fc5afda0
Makkenzie <makkenzie1@ymail.com>
2022-05-03 14:20:51 -0700

    edited html and js file

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -10,7 +10,8 @@ var outputEl = document.getElementById("output");
 
 //creating a new element and assigning it to a new variable.
 var new1El = document.createElement("p");
-new1El.innerHTML = "say something new";
+new1El.innerHTML = "<u>new1El: </u>" + "<br>" + "say something new </br>";
+
 
 
 //edits to the new element text
@@ -18,9 +19,10 @@ new1El.style.textAlign = "center";
 new1El.style.fontSize = "35pt";
 
 
-//creating a secind new element and assigning it to a new variable.
+
+//creating a second new element and assigning it to a new variable.
 var new2El = document.createElement("p");
-new2El.innerHTML = "new2El: " + "<bre>" + "say something else </br>";
+new2El.innerHTML = "<u>new2El: </u>" + "<br>" + "say something else </br>";
 
 
 //edits to the new element text
@@ -37,13 +39,36 @@ outputEl.appendChild(new2El);
 var xBonus = document.createElement("p");
 xBonus.innerHTML = "X Bonus Task 1";
 outputEl.insertBefore(xBonus, outputEl.children[0]);
+
+//edits the X Bonus task text
 xBonus.style.fontSize = "30pt";
+xBonus.style.textDecoration = "underline";
 
 //edited the main header
 document.getElementById("mainHeader").style.fontFamily = "Papyrus, fantasy";
 
-//edits the header challenges
+//edits the challenges header
 document.getElementById("challenges").style.fontFamily = "Papyrus, fantasy";
+document.getElementById("challenges").style.color = "IndianRed";
+
+//edits the problems header
+document.getElementById("problems").style.color = "Teal";
+
+//edits the results header
+document.getElementById("results").style.color = "Purple";
+
+//edits my paragraphs
+document.getElementById("paragraph").style.color = "Black";
+document.getElementById("paragraph").style.fontFamily = "Verdana, sans-serif";
+
+document.getElementById("paragraph01").style.color = "Black";
+document.getElementById("paragraph01").style.fontFamily = "Verdana, sans-serif";
+
+document.getElementById("paragraph02").style.color = "Black";
+document.getElementById("paragraph02").style.fontFamily = "Verdana, sans-serif";
+
+document.getElementById("paragraph03").style.color = "Black";
+document.getElementById("paragraph03").style.fontFamily = "Verdana, sans-serif";
 
 //Changes to my <div> output
 document.getElementById("output").style.color = "DarkSlateGray";
