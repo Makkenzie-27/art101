da155d82a4069a6c2a6add791fc98a5b6667d1e2
Makkenzie <makkenzie1@ymail.com>
2022-05-08 18:58:04 -0700

    edited lab css, js, and html

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -4,3 +4,16 @@
  *Lab 10: JavaScript Events and Forms
  *License: Public Domain
  */
+
+ var buttonEl = document.getElementById("my-button");
+
+ buttonEl = addEventListener("click", function(){
+   console.log("click");
+   var nameInput = document.getElementById("name").value;
+   var outputEl = document.getElementById("output");
+
+   var split = nameInput.split('');
+   split.sort();
+   var joined = split.join('');
+   outputEl.innerHTML = (joined);
+ })
