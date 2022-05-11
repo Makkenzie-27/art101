1c8d8735dd8e16fd24d49985c0ca228763ebdbfb
Makkenzie <makkenzie1@ymail.com>
2022-05-11 10:50:26 -0700

    edited js, css, and html

--- lab11/js/lab.js
+++ lab11/js/lab.js
@@ -9,33 +9,50 @@
  challengeEl = $("#challenge");
  problemsEl = $("#problems");
  resultsEl = $("#results");
+ bonusEl = $("#output");
 
  challengeEl.append("<button id='c-button'>Click me!</button>");
  problemsEl.append("<button id='p-button'>Click me!</button>");
  resultsEl.append("<button id='r-button'>Click me!</button>");
+ bonusEl.append("<button id='bonus-button'>Click me!</button>");
 
-//hover color effect / box change
- $("#c-button").hover(function(){
-  $("#c-button").toggleClass("hoverColor");
+//hover color effect / box change when the Challenge button is pressed:
+
+
+$("#c-button").hover(function(){
+  $("#c-button").toggleClass("hoverOne");
 })
 $("#c-button").click(function(){
-  challengeEl.toggleClass("special");
+  challengeEl.toggleClass("changeOne");
   $("#c-button").toggleClass("buttonOne");
 })
 
+//Problems button is pressed:
+
 $("#p-button").hover(function(){
- $("#p-button").toggleClass("hoverColor");
+  $("#p-button").toggleClass("hoverTwo");
 })
 $("#p-button").click(function(){
- problemsEl.toggleClass("special");
- $("#p-button").toggleClass("buttonTwo");
+  problemsEl.toggleClass("changeTwo");
+  $("#p-button").toggleClass("buttonTwo");
 })
 
+//Results button is pressed:
 
 $("#r-button").hover(function(){
- $("#r-button").toggleClass("hoverColor");
+  $("#r-button").toggleClass("hoverThree");
 })
 $("#r-button").click(function(){
- resultsEl.toggleClass("special");
- $("#r-button").toggleClass("buttonThree");
+  resultsEl.toggleClass("changeThree");
+  $("#r-button").toggleClass("buttonThree");
+})
+
+//X Bonus Task:
+
+$("#bonus-button").hover(function(){
+  $("#bonus-button").toggleclass("buttonFour")
+})
+$("#bonus-button").click(function(){
+  $(":button").toggleClass("buttonOne");
+  $(".sectionbox").toggleClass("changeOne");
 })
