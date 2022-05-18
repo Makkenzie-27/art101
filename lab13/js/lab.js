7273939675965301198eace019f09ba4c45357d8
Makkenzie <makkenzie1@ymail.com>
2022-05-18 08:14:09 -0700

    edited the css and js

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -13,58 +13,94 @@ function fizzBuzzBoom(){
   var fbb = "";
   for (var i=1; i <= 200; i++){
 
-     if ((i % 3 == 0)&&(i % 5 == 0)&&(i % 7 == 0)){
-       console.log("FizzBuzzBoom");
-       fbb += (i + " FizzBuzzBoom!<br>");
+     if ((i % 3 == 0)&&(i % 5 == 0)&&(i % 7 == 0)&&(i % 8 == 0)){
+       console.log("FizzBuzzBoomBang");
+       fbb += (i + " FizzBuzzBoomBang!<br><br>");
      }
 
      else if (i % 3 == 0){
        console.log("Fizz");
-       fbb += (i + " Fizz!<br>");
+       fbb += (i + " Fizz!<br><br>");
      }
+
      else if ((i % 3 == 0)&&(i % 5 == 0)){
        console.log("FizzBuzz");
-       fbb += (i + " FizzBuzz!<br>");
+       fbb += (i + " FizzBuzz!<br><br>");
      }
 
      else if ((i % 3 == 0)&&(i % 7 == 0)){
        console.log("FizzBoom");
-       fbb += (i + " FizzBoom!<br>");
+       fbb += (i + " FizzBoom!<br><br>");
+     }
+
+     else if ((i % 3 == 0)&&(i % 8 == 0)){
+       console.log("FizzBang");
+       fbb += (i + " FizzBang!<br><br>");
      }
 
      else if (i % 5 == 0){
        console.log("Buzz");
-       fbb += (i + " Buzz!<br>");
+       fbb += (i + " Buzz!<br><br>");
      }
 
      else if ((i % 5 == 0)&&(i % 3 == 0)){
        console.log("BuzzFizz");
-       fbb += (i + " BuzzFizz!<br>");
+       fbb += (i + " BuzzFizz!<br><br>");
      }
 
      else if ((i % 5 == 0)&&(i % 7 == 0)){
        console.log("BuzzBoom");
-       fbb += (num + " BuzzBoom!<br>");
+       fbb += (num + " BuzzBoom!<br><br>");
+     }
+
+     else if ((i % 5 == 0)&&(i % 8 == 0)){
+       console.log("BuzzBang");
+       fbb += (i + " BuzzBang!<br><br>");
      }
 
      else if (i % 7 == 0){
        console.log("Boom");
-       fbb += (i + " Boom!<br>");
+       fbb += (i + " Boom!<br><br>");
      }
 
      else if ((i % 7 == 0)&&(i % 3 == 0)){
        console.log("BoomFizz");
-       fbb += (i + " BoomFizz!<br>");
+       fbb += (i + " BoomFizz!<br><br>");
      }
 
      else if ((i % 7 == 0)&&(i % 5 == 0)){
        console.log("BoomBuzz");
-       fbb += (i + " BoomBuzz!<br>");
+       fbb += (i + " BoomBuzz!<br><br>");
+     }
+
+     else if ((i % 7 == 0)&&(i % 8 == 0)){
+       console.log("BoomBang");
+       fbb += (i + " BoomBang!<br><br>");
+     }
+
+     else if (i % 8 == 0){
+       console.log("Bang");
+       fbb += (i + " Bang!<br><br>");
+     }
+
+     else if ((i % 8 == 0)&&(i % 3 == 0)){
+       console.log("BangFizz");
+       fbb += (i + " BangFizz!<br><br>");
+     }
+
+     else if ((i % 8 == 0)&&(i % 5 == 0)){
+       console.log("BangBuzz");
+       fbb += (i + " BangBuzz!<br><br>");
+     }
+
+     else if ((i % 8 == 0)&&(i % 7 == 0)){
+       console.log("BangBoom");
+       fbb += (i + " BangBoom!<br><br>");
      }
 
      else {
        console.log(i);
-       fbb += (i + "<br>");
+       fbb += (i + "<br><br>");
      }
  }
   return fbb;
