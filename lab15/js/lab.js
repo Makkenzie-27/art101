3667301039f44bd6b9bea29a23a0407585271090
Makkenzie <makkenzie1@ymail.com>
2022-05-25 10:41:36 -0700

    editing the js file for lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -4,3 +4,34 @@
  *Lab 15: AJAX
  *License: Public Domain
  */
+
+
+
+ // var endpoint = "http://www.yerkee.com/api";
+ var endpoint = "http://www.yerkee.com/";
+
+ function getAjax() {
+ 	$.ajax({
+   	url: endpoint,
+    //ant data to send
+    data: { id: 123},
+    //POST or GET request
+    type: "GET",
+    //data type we expect back
+    dataType: "json",
+   })
+   .done(function(data){
+   	console.log("Worked!", data);
+   	$("#output").append("<h3>" + data.title + "</h3>");
+   	$("#output").append("<p>" + data.alt);
+    $(selector).get(URL, callback);
+    $(selector).post(URL, data, callback);
+    $(selector).load(URL, data, callback);
+   })
+   .fail(function(request, error){
+     console.log(request, error);
+   	$("#output").html("Something Isn't Working.");
+   })
+ }
+
+ $("button").click(getAjax);
