20daa3be30c71646dbd4e46809435533309fe897
Makkenzie <makkenzie1@ymail.com>
2022-05-26 11:37:13 -0700

    edited lab 15 css, and html

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -7,31 +7,61 @@
 
 
 
- // var endpoint = "http://www.yerkee.com/api";
- var endpoint = "http://www.yerkee.com/";
 
- function getAjax() {
- 	$.ajax({
-   	url: endpoint,
-    //ant data to send
-    data: { id: 123},
-    //POST or GET request
+// var endpoint = "https://yesno.wtf/#api";
+var endpoint = "https://yesno.wtf/api";
+
+function getAjax() {
+	$.ajax({
+  	url: endpoint,
     type: "GET",
-    //data type we expect back
-    dataType: "json",
-   })
-   .done(function(data){
-   	console.log("Worked!", data);
-   	$("#output").append("<h3>" + data.title + "</h3>");
-   	$("#output").append("<p>" + data.alt);
-    $(selector).get(URL, callback);
-    $(selector).post(URL, data, callback);
-    $(selector).load(URL, data, callback);
-   })
-   .fail(function(request, error){
-     console.log(request, error);
-   	$("#output").html("Something Isn't Working.");
-   })
- }
 
- $("button").click(getAjax);
+  })
+  .done(function(data){
+  	console.log("Worked!");
+    var answer = data.answer;
+    var imgUrl = data.image;
+    console.log(answer, imgUrl);
+  	$("#output").html("<h3>" + answer + "</h3>");
+  	$("#output").append("<img src=" + imgUrl + ">");
+
+  })
+  .fail(function(request, error){
+  	$("#output").html("Ah Ah Ah, You Didn't Use the Magic Word");
+  })
+}
+
+$("button").click(getAjax);
+
+
+ // // var endpoint = "http://numbersapi.com/42/";
+ // // var endpoint = "https://xkcd.com/info.0.json";
+ // var endpoint = "https://api.nasa.gov/planetary/apod";
+ //
+ // function getAjax() {
+ // 	$.ajax({
+ //   	url: endpoint,
+ //     type: "GET",
+ //     data: {
+ //     	api_key: "0JpHHNGBd500KLTVOjR6w3nq4AOVP3kSsvzZtgxK",
+ //       count: 1
+ //     }
+ //   })
+ //   .done(function(data){
+ //   	console.log("Worked!");
+ //     var firstAPOD = data[0];
+ //     console.log(firstAPOD);
+ //     var title = firstAPOD.title;
+ //     var descr = firstAPOD.explanation;
+ //     var imgUrl = firstAPOD.url;
+ //     console.log(title, descr, imgUrl);
+ //   	$("#output").html("<h3>" + title + "</h3>");
+ //   	$("#output").append("<img src=" + imgUrl + ">");
+ //   	$("#output").append("<p>" + descr + "</p>");
+ //   })
+ //   .fail(function(request, error){
+ //   	$("#output").html("Something fucked up.");
+ //   })
+ // }
+
+ //$("button").click(getAjax);
